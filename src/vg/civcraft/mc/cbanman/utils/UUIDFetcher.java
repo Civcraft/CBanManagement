3882e8a6bbcf4e0cad7bd52553048e3d189b0892
Suirad <suirad@users.noreply.github.com>
2015-09-27 23:35:52 +0300

    Add url to uuidfetcher

--- src/vg/civcraft/mc/cbanman/utils/UUIDFetcher.java
+++ src/vg/civcraft/mc/cbanman/utils/UUIDFetcher.java
@@ -18,6 +18,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ * 
+ * URL: https://gist.github.com/evilmidget38/26d70114b834f71fb3b4
  */
 package vg.civcraft.mc.cbanman.utils;
 
